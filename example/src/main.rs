b2d165aa66e369feb785c282078ca15c76125458
KosekiDev <vincent.brehaut@gmail.com>
2021-06-30 00:11:54 +0200

    Rename exemple folder to example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,35 +1,39 @@
 use actix_web::{post, App, HttpResponse, HttpServer};
 use serde::{Deserialize};
+use actix_multipart::Multipart;
 
 use actix_extract_multipart::*;
 
 #[derive(Deserialize)]
-struct Example {
+struct Exemple {
     string_param: String,
     optional_u_param: Option<u32>,
-    files_param: Option<Vec<File>>
+    file_param: Option<File>
 }
 
-fn saving_files_function(file: &Vec<File>) -> Result<(), ()> {
+fn saving_file_function(file: File) -> Result<(), ()> {
     // Do some stuff here
-    for f in file {
-        println!("Saving file \"{}\" successfully (type: {:?})", f.name(), f.file_type());
-    }
+    println!("Saving file \"{}\" successfully", file.filename);
 
     Ok(())
 }
 
-#[post("/example")]
-async fn index(example_structure: Multipart::<Example>) -> HttpResponse {
-    println!("Value of string_param: {}", example_structure.string_param);
-    println!("Value of optional_u_param: {:?}", example_structure.optional_u_param);
-    println!("Having files? {}", match &example_structure.files_param {
+#[post("/exemple")]
+async fn index(payload: Multipart) -> HttpResponse {
+    let exemple_structure = match extract_multipart::<Exemple>(payload).await {
+        Ok(data) => data,
+        Err(_) => return HttpResponse::BadRequest().json("The data received does not correspond to those expected")
+    };
+    
+    println!("Value of string_param: {}", exemple_structure.string_param);
+    println!("Value of optional_u_param: {:?}", exemple_structure.optional_u_param);
+    println!("Having file? {}", match exemple_structure.file_param {
         Some(_) => "Yes",
         None => "No"
     });
 
-    if let Some(file) = &example_structure.files_param {
-        match saving_files_function(&file) {
+    if let Some(file) = exemple_structure.file_param {
+        match saving_file_function(file) {
             Ok(_) => println!("File saved!"),
             Err(_) => println!("An error occured while file saving")
         }
